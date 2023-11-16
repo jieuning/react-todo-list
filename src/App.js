9e4b76611c5957b26b49680009c157adcc91d33b
JIEUN <balllling92@gmail.com>
2023-11-16 10:10:14 +0900

    FEAT: todo list 완성

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
+import React, { useMemo, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [filter, setFilter] = useState("all");
+
+  const [inputValue, setInputValue] = useState("");
+
+  const filteredItems = useMemo(() => {
+    if (filter === 'active') {
+      return items.filter(item => {
+        return !item.isCompleted;
+      })
+    } else if (filter === 'completed') {
+      return items.filter(item => {
+        return item.isCompleted;
+      })
+    } else {
+      return items;
+    }
+  }, [items, filter])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container">
+      <input
+        type="text"
+        id="todo-input"
+        placeholder="할 일 추가"
+        value={inputValue}
+        onChange={event => setInputValue(event.target.value)}
+      />
+      <button id="add-todo" onClick={() => {
+        setItems([
+          ...items,
+          {
+            id: items.length,
+            content: inputValue,
+            isCompleted: false,
+          },
+        ]);
+        // 초기화
+        setInputValue("");
+      }}
+      >추가</button>
+      <div class="spacing-10"></div>
+      <div class="filters">
+        <button id="filter-all" onClick={() => {
+          setFilter("all")
+        }}>전체</button>
+        <button id="filter-active" onClick={() => {
+          setFilter("active")
+        }}>진행 중</button>
+        <button id="filter-completed" onClick={() => {
+          setFilter("completed")
+        }}>완료됨</button>
+      </div>
+      <ul id="todo-list">
+        {filteredItems.map((item, index) => {
+          return (
+            <li
+              key={item.id}
+              style={{
+                textDecoration: item.isCompleted ? "line-through" : "none"
+              }}
+              onClick={() => {
+                const newItems = [...items];
+                newItems[index].isCompleted = !newItems[index].isCompleted;
+                setItems(newItems);
+              }}
+            >{item.content}</li>
+          )
+        })}
+      </ul>
     </div>
   );
 }
